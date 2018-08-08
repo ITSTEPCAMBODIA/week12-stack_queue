d0cf53eaea938a7e27766a0d36474d07f15fba37
SrengMSI <srengty@gmail.com>
2018-08-08 10:17:09 +0700

    add try catch

--- Exceptions/TestException.cpp
+++ Exceptions/TestException.cpp
@@ -1,14 +1,29 @@
 #include<iostream>
 using namespace std;
+void addition(int a, int b) {
+	if (b == 0) throw 0;
+	if (b > a) throw "b must be smaller than a.\n";
+	int z = a / b;
+	cout << "Result: " << z << endl;
+}
 int main() {
 	int a, b;
 	cout << "Input a b: "; cin >> a >> b;
+	//addition(a, b);
 	try {
-		if (b == 0) throw 0;
-		int z = a / b;
-		cout << "Result: " << z << endl;
+		//addition(a, b);
+		int z = a + b;
+		throw 2.0;
 	}
 	catch (int c) {
 		cout << "Division by " << c << endl;
+		//throw;
 	}
+	catch (char*str) {
+		cout << "ERROR: "<<b<<" should be less than "<<a<<endl;
+	}
+	catch (...) {
+
+	}
+	system("pause");
 }
