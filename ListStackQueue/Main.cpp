df4b0cb8ee2ccc6cca007c3b564c6655c370a463
SrengMSI <srengty@gmail.com>
2018-08-08 07:45:43 +0700

    complete remainings

--- ListStackQueue/Main.cpp
+++ ListStackQueue/Main.cpp
@@ -2,14 +2,25 @@
 #include "Stack.h"
 #include "Queue.h"
 using namespace std;
-
-int main() {
-	cout << "Surprise mother f***";
-	List<int> *l = new Stack<int>;
+void testList(List<int>*l) {
 	l->insert(1);
 	l->eject();
 	l->insert(2);
 	l->insert(5);
 	l->insert(8);
+	l->display();
+	l->eject();
+	l->display();
+	l->eject();
+	l->eject();
+	l->eject();
+	l->eject();
+	l->display();
+}
+int main() {
+	List<int> *l = new Stack<int>;
+	cout << "Stack:\n"; testList(l);
+	l = new Queue<int>;
+	cout << "Queue:\n"; testList(l);
 	system("pause");
 }
--- ListStackQueue/Queue.h
+++ ListStackQueue/Queue.h
@@ -10,3 +10,39 @@ public:
 	virtual void eject();//Da veat hello
 };
 
+template<class T>
+inline Queue<T>::Queue(){}
+
+template<class T>
+inline Queue<T>::~Queue()
+{
+}
+
+template<class T>
+inline void Queue<T>::insert(T t)
+{
+	T *tmp = this->ts;
+	this->ts = new T[this->count + 1];
+	for (int i = 0; i < this->count; i++)this->ts[i] = tmp[i];
+	this->ts[this->count] = t;
+	this->count++;
+	delete[]tmp;
+}
+
+template<class T>
+inline void Queue<T>::eject()
+{
+	if (this->count > 0) {
+		if (this->count == 1) {
+			delete[]this->ts;
+			this->ts = NULL;
+		}
+		else {
+			T *tmp = this->ts;
+			this->ts = new T[this->count - 1];
+			for (int i = this->count - 1; i > 0; i--) this->ts[i - 1] = tmp[i];
+			delete[]tmp;
+		}
+		this->count--;
+	}
+}
